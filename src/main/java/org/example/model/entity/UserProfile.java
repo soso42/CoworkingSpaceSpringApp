f0f0bcc51a68fc76949626aabaa67a418563d060
Soso42 <sosojanashvili@gmail.com>
2025-07-08 02:10:45 +0400

    added UserProfile Entity

---
+++ src/main/java/org/example/model/entity/UserProfile.java
@@ -0,0 +1,30 @@
+package org.example.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usr")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
